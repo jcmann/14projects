7d43233dabcb3e797a1d70516be34a23042fc505
Jen Mann <jen.c.mann@gmail.com>
2021-09-26 21:11:15 -0500

    Add insert, update, delete methods

--- src/main/java/com/jenmann/persistence/CharacterDao.java
+++ src/main/java/com/jenmann/persistence/CharacterDao.java
@@ -19,13 +19,41 @@ public class CharacterDao {
     SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
 
     public List<Character> getAllCharacters() {
+        logger.info("Getting all characters...");
         Session session = sessionFactory.openSession();
         CriteriaBuilder builder = session.getCriteriaBuilder();
         CriteriaQuery<Character> query = builder.createQuery(Character.class);
         Root<Character> root = query.from(Character.class);
         List<Character> characters = session.createQuery(query).getResultList();
         session.close();
+        logger.info("Returning all characters.");
         return characters;
+    }
+
+    public void saveOrUpdate(Character character) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.saveOrUpdate(character);
+        transaction.commit();
+        session.close();
+    }
+
+    public int insert(Character character) {
+        int id = 0;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        id = (int)session.save(character);
+        transaction.commit();
+        session.close();
+        return id;
+    }
+
+    public void delete(Character character) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(character);
+        transaction.commit();
+        session.close();
     }
 
 
