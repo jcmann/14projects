6abdc77186749bcd6714277e497c035e3869b630
Jen Mann <jen.c.mann@gmail.com>
2021-09-26 21:42:33 -0500

    Add a cleandb script to reset the test database

---
+++ src/test/resources/cleandb.sql
@@ -0,0 +1,8 @@
+delete from characters;
+insert into characters (name, level, race, character_class)
+    values("Rigby", 5, "Tabaxi", "Bard");
+insert into characters (name, level, race, character_class)
+    values("Tank", 2, "Kobold", "Monk");
+insert into characters (name, level, race, character_class)
+    values("Teeko", 15, "Dragonborn", "Paladin");
+
