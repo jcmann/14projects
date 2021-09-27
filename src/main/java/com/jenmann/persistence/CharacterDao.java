8165380fdbb84def088c069a6fd4c7b706e9ce57
Jen Mann <jen.c.mann@gmail.com>
2021-09-26 20:21:07 -0500

    Add base dao implementation file with imports from demo code

---
+++ src/main/java/com/jenmann/persistence/CharacterDao.java
@@ -0,0 +1,19 @@
+package com.jenmann.persistence;
+
+import com.jenmann.entity.Character;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class CharacterDao {
+
+
+}
