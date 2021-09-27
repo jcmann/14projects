7fa393bc81f66247782f60892d5a455c22779ab3
Jen Mann <jen.c.mann@gmail.com>
2021-09-26 20:35:23 -0500

    Add draft getAll method

--- src/main/java/com/jenmann/persistence/CharacterDao.java
+++ src/main/java/com/jenmann/persistence/CharacterDao.java
@@ -15,5 +15,18 @@ import java.util.List;
 
 public class CharacterDao {
 
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
+
+    public List<Character> getAllCharacters() {
+        Session session = sessionFactory.openSession();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<Character> query = builder.createQuery(Character.class);
+        Root<Character> root = query.from(Character.class);
+        List<Character> characters = session.createQuery(query).getResultList();
+        session.close();
+        return characters;
+    }
+
 
 }
