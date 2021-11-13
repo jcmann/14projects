89b51d51038360e1dfd557dc4b72a2089024de22
Jen Mann <jen.c.mann@gmail.com>
2021-11-12 19:52:03 -0600

    Add Encounter class with annotations and 1:M relationship

--- src/main/java/com/jenmann/entity/Encounter.java
+++ src/main/java/com/jenmann/entity/Encounter.java
@@ -1,9 +1,74 @@
 package com.jenmann.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity(name = "Encounter")
+@Table(name = "encounters")
 public class Encounter {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
     private int id;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "difficulty")
     private String difficulty;
 
+    @OneToMany(mappedBy = "encounter", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    private Set<Characters> encounterCharacters = new HashSet<Characters>(0);
+
+    public Encounter() {
+    }
+
+    public Encounter(String title, String difficulty, Set<Characters> encounterCharacters) {
+        this.title = title;
+        this.difficulty = difficulty;
+        this.encounterCharacters = encounterCharacters;
+    }
+
+    public Encounter(int id, String title, String difficulty, Set<Characters> encounterCharacters) {
+        this.id = id;
+        this.title = title;
+        this.difficulty = difficulty;
+        this.encounterCharacters = encounterCharacters;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Characters> getEncounterCharacters() {
+        return encounterCharacters;
+    }
+
+    public void setEncounterCharacters(Set<Characters> encounterCharacters) {
+        this.encounterCharacters = encounterCharacters;
+    }
 }
