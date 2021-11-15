7d1539a1891e204e4a64708deb25b20cc066992e
Jen Mann <jen.c.mann@gmail.com>
2021-11-14 19:23:41 -0600

    Clean up comments/javadoc

--- src/main/java/com/jenmann/controller/EncounterAPI.java
+++ src/main/java/com/jenmann/controller/EncounterAPI.java
@@ -6,7 +6,6 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jenmann.entity.Characters;
 import com.jenmann.entity.Encounter;
 import com.jenmann.persistence.EncounterDao;
 import org.apache.logging.log4j.LogManager;
@@ -38,6 +37,11 @@ public class EncounterAPI {
      */
     private final Logger logger = LogManager.getLogger(this.getClass());
 
+    /**
+     * This endpoint returns all encounters in the database.
+     *
+     * @return stringified JSON of all encounters in the db
+     */
     @GET
     @Produces("application/json")
     public Response getAllEncounters() {
@@ -53,7 +57,12 @@ public class EncounterAPI {
         return Response.status(200).entity(responseJSON).build();
     }
 
-    // get by encounter id
+    /**
+     * This endpoint returns a single encounter by the ID provided.
+     *
+     * @param id the id of the encounter to search for
+     * @return stringified JSON of that encounter, or a failed status
+     */
     @GET
     @Path("{id}")
     @Produces("application/json")
@@ -64,9 +73,10 @@ public class EncounterAPI {
         try {
             responseJSON = getObjectMapper().writeValueAsString(encounter);
         } catch (Exception e) {
-            logger.error("", e); // TODO clean up logs
+            logger.error("", e);
         }
 
+        // TODO conditionally render to make sure null is a 500 or something
         return Response.status(200).entity(responseJSON).build();
     }
 
