b7e6cd449a5544d1f80392bf6884dcc5aff157fe
Jen Mann <jen.c.mann@gmail.com>
2021-12-09 23:14:26 -0600

    Add getMonsterByIndexSuccess test

--- src/test/java/com/jenmann/persistence/MonsterDaoTest.java
+++ src/test/java/com/jenmann/persistence/MonsterDaoTest.java
@@ -1,6 +1,7 @@
 package com.jenmann.persistence;
 
 import com.jenmann.entity.GetAllResponse;
+import com.jenmann.entity.Monster;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
@@ -19,15 +20,27 @@ public class MonsterDaoTest {
     private final Logger logger = LogManager.getLogger(this.getClass());
 
     /**
+     * A MonsterDao that is re-initialized before each test.
+     */
+    private MonsterDao dao = new MonsterDao();
+
+    /**
      * This tests the getAllMonsters method of the MonsterDao. This would hypothetically require
      * regular updates if the API were to be changed, as the count or alphabetical index may change.
      */
     @Test
-    void getAllMonstersSuccess() {
+    public void getAllMonstersSuccess() {
         logger.info("Starting test getAllMonstersSuccess");
-        MonsterDao dao = new MonsterDao();
         GetAllResponse gar = dao.getAllMonsters();
         assertEquals(332, gar.getCount());
         assertEquals("Aboleth", gar.getResults().get(0).getName());
+    }
+
+    @Test
+    public void getMonsterByIndexSuccess() {
+        logger.info("Starting test getMonsterByIndexSuccess.");
+        Monster aboleth = dao.getMonsterByIndex("aboleth");
+        assertEquals("Aboleth", aboleth.getName());
+        assertEquals("lawful evil", aboleth.getAlignment());
     }
 }
